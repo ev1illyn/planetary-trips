a2f5d0c7c2ba2638023922a3e054dc3df71e4867
ev1illyn <evillyndsoliveiras@gmail.com>
2021-01-21 16:57:29 -0300

    feat: add default repositories

--- src/main/java/com/spacex/model/Flight.java
+++ src/main/java/com/spacex/model/Flight.java
@@ -4,12 +4,9 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
---
+++ src/main/java/com/spacex/repository/Local.java
@@ -0,0 +1,12 @@
+package com.spacex.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spacex.model.Airport;
+
+@Repository
+public interface Local extends JpaRepository<Airport, Long>{
+
+}
+
