91e9120212b11919406cbd96a8c406dd4a48e844
ev1illyn <evillyndsoliveiras@gmail.com>
2021-01-25 23:28:42 -0300

    feat: add sql data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO LOCAL(id, zip_code, country, state, city)
+	VALUES(10, '63020-735', 'Brasil', 'CE', 'Juazeiro do Norte');
+	
+INSERT INTO LOCAL(id, zip_code, country, state, city)
+	VALUES(20, '60741-900', 'Brasil', 'CE', 'Fortaleza');
+	
+INSERT INTO AIRPORT(id, name, address_id, street)
+	VALUES(10, 'Aeroporto de Juazeiro do Norte - Orlando Bezerra de Menezes',
+	10, 'Av. Virgílio Távora, 4000 - Aeroporto');
+	
+INSERT INTO AIRPORT(id, name, address_id, street)
+	VALUES(20, 'Aeroporto Internacional de Fortaleza - Pinto Martins',
+	20, 'Av. Senador Carlos Jereissati, 3000 - Serrinha');
