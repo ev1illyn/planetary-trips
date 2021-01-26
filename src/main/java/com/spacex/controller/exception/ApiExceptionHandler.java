37c0fb029a2531a0ebb5825c73b8ae4e09047b92
ev1illyn <evillyndsoliveiras@gmail.com>
2021-01-25 23:27:41 -0300

    feat: add exception handler

---
+++ src/main/java/com/spacex/controller/exception/ApiExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.spacex.controller.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// interceptor - spring component to handle exceptions
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<FormErrorDto> exceptionHandler(MethodArgumentNotValidException exception) {
+
+		List<FormErrorDto> dto = new ArrayList<>();
+
+		List<FieldError> invalidFields = exception.getBindingResult().getFieldErrors();
+
+		invalidFields.forEach(e -> {
+			String message = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+			FormErrorDto formErrorDto = new FormErrorDto(e.getField(), message);
+			dto.add(formErrorDto);
+		});
+
+		return dto;
+
+	}
+}
---
+++ src/main/java/com/spacex/controller/exception/FormErrorDto.java
@@ -0,0 +1,23 @@
+package com.spacex.controller.exception;
+
+public class FormErrorDto {
+	
+	private String field;
+	
+	private String error;
+
+	public String getField() {
+		return field;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public FormErrorDto(String field, String error) {
+		super();
+		this.field = field;
+		this.error = error;
+	}
+	
+}
