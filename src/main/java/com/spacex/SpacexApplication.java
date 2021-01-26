da70ecb986ef2d0ae0c09b71803f78bf7e4f3344
ev1illyn <evillyndsoliveiras@gmail.com>
2021-01-25 23:29:39 -0300

    feat: add annotations

--- src/main/java/com/spacex/SpacexApplication.java
+++ src/main/java/com/spacex/SpacexApplication.java
@@ -2,8 +2,14 @@ package com.spacex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableJpaRepositories(basePackages = {"com.spacex"})
+@EnableCaching
 public class SpacexApplication {
 
 	public static void main(String[] args) {
